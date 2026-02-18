e5b2f001409e22eef41694ccbeb4f903c0c572d1
ahmedali839 <ahmedyarr1212@gmail.com>
2026-02-19 01:41:30 +0500

    learning fully route HTML page cache _and _ router cache

--- cache/app/page.js
+++ cache/app/page.js
@@ -6,21 +6,25 @@ export default function Home() {
   return (
     <div className="block  p-40 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hi, Yar! let's dive deep to Cache topic </h1>
-      <p>
-        <b>/: Explains Full route Cache "Entire HTML rendered page" </b> It's
-        client page, using current time but not updating on page refresh cause
-        this HTML page cached for next 10s {new Date().toISOString()}
+      <p className="m-5">
+        <b>
+          /: Explains Full route Cache "Entire HTML rendered page" Only work in
+          production mode not npm run dev{" "}
+        </b>{" "}
+        Actually It's client page, using current time but not updating on page
+        refresh cause this HTML page cached for next 10s:{" "}
+        {new Date().toISOString()}
       </p>
-      <p>
+      <p className="m-5">
         {" "}
-        <b>/dashboard: Explains Data fetch</b> fetching current time from server
-        but it cached for 10s, after cached time it'll get current data from
-        server
+        <b>/dashboard: Explains Data fetch </b> fetching current time from
+        server but it cached for 10s, after cached time it'll get current data
+        from server
       </p>
-      <p>
+      <p className="m-5">
         {" "}
         <b>/products: Explains route-component cache</b> Navigating to back page
-        'already rendered' more smoothness, user feel like not-navigated, not it
+        'already rendered' more smoothness, user feel like not-navigated, but it
         is navigated but fast and smooth
       </p>
     </div>
