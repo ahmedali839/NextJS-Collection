1bbe22fea240ca70fe059fe23c3f04be670ef91f
ahmedali839 <ahmedyarr1212@gmail.com>
2026-02-24 23:53:12 +0500

    lazy-loading

--- lazy-loading/app/componets/c.jsx
+++ lazy-loading/app/componets/c.jsx
@@ -10,13 +10,7 @@ export default function C() {
     setMounted(true);
   }, []);
 
-  useEffect(() => {
-    if (mounted) {
-      setTimeout(() => {
-        setMounted1(true);
-      }, 2000);
-    }
-  }, [mounted]);
+  
 
   return (
     <>
