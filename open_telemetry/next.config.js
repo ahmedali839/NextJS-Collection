865c19b4bc0512cc76e657aabd6051c4f21ab450
Ahmed <ahmedyarr1212@gmail.com>
2026-03-04 23:59:10 +0500

    open_telemetry

--- open_telemetry/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    instrumentationHook: true, // This is the "On" switch
-  },
-};
-
-module.exports = nextConfig;
