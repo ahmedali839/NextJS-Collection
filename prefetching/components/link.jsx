de0b20e67967bb1f280d32faef6b55349c05bf87
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-09 23:58:26 +0500

    prefetching 02

---
+++ prefetching/components/link.jsx
@@ -0,0 +1,18 @@
+// components/link.jsx
+import Link from "next/link";
+import { useState } from "react";
+
+export function CustomLink({ href, children, className, ...props }) {
+  const [active, setActive] = useState(false);
+  return (
+    <Link
+      href={href}
+      prefetch={active ? null : false} 
+      // We disable automatic prefetching here!
+      className={`font-bold text-blue-600 hover:text-blue-800 transition ${className}`}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
