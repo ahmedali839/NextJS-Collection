284fa9264fe2e4055ca61dcbfa2e1ae91b0035c1
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-09 23:57:30 +0500

    prefetching 01

--- prefetching/components/link.jsx
+++ prefetching/components/link.jsx
@@ -9,6 +9,7 @@ export function CustomLink({ href, children, className, ...props }) {
       href={href}
       prefetch={active ? null : false} 
       // We disable automatic prefetching here!
+      // solved
       className={`font-bold text-blue-600 hover:text-blue-800 transition ${className}`}
       {...props}
     >
