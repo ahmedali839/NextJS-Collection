9e3fe4865835c0f080fb73fbfcf9505d099703ba
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-11 23:48:46 +0500

    script 02

--- script/app/page.jsx
+++ script/app/page.jsx
@@ -3,8 +3,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screexn w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h2>Hello, Yar...!</h2>
+        <h2>Hello, Yar...!</h2> 
         <h4>Today, Yar is learning Optimized Script in Next.JS </h4>
+        <h4>Navigate to "/script" to see how Script tag working</h4>
       </main>
     </div>
   );
