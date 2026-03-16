e58ba48d02863cd3def6e6f242a642c1f913955d
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-16 18:18:04 +0500

    third-party-libraries

---
+++ third_party_libraries/app/page.js
@@ -0,0 +1,13 @@
+import { GoogleTagManager } from "@next/third-parties/google";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <GoogleTagManager gtmId="GTM-XYZ" />
+      <h2>
+        Hello Yar! Working with third-party-libraries in Next.JS on
+        16-March-2026{" "}
+      </h2>
+    </div>
+  );
+}
