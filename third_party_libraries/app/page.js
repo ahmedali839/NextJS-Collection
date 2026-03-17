d9a8132176be862352716a83537746c9faf7233a
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-17 23:54:59 +0500

    third-party-libraries - google_tagManager

--- third_party_libraries/app/page.js
+++ third_party_libraries/app/page.js
@@ -1,51 +1,3 @@
-// import { GoogleTagManager } from "@next/third-parties/google";
-
-// export default function Home() {
-//   return (
-//     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-//       <GoogleTagManager gtmId="GTM-XYZ" />
-//       <h2>
-//         Hello Yar! Working with third-party-libraries in Next.JS on the
-//         16-March-2026{" "}
-//       </h2>
-//     </div>
-//   );
-// }
-
-// 'use client';
-
-// import EventButton from './components/sendGAEvent';
-
-// export default function Page() {
-//   return (
-//     <div>
-//       <h2>Welcome to the Event Tracker</h2>
-//       <EventButton />
-//     </div>
-//   );
-// }
-
-// 'use client';
-
-// import { useEffect } from 'react';
-// import EventButton from './components/sendGAEvent';
-
-// export default function Page() {
-//   useEffect(() => {
-//     // Only run GA setup on client side
-//     if (typeof window !== 'undefined' && window.gtag) {
-//       window.gtag('config', 'GTM-5ZHL2HQ9'); // Ensure it runs on client
-//     }
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Welcome to the Event Tracker</h2>
-//       <EventButton />
-//     </div>
-//   );
-// }
-
 import EventButton from "./components/sendGAEvent";
 
 export default function Page() {
@@ -56,3 +8,4 @@ export default function Page() {
     </div>
   );
 }
+ 
