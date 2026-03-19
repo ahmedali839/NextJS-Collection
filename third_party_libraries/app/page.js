dd7ad377fb3cafdd914ff50a0945b65ba49b51e9
ahmedali839 <ahmedyarr1212@gmail.com>
2026-03-19 23:27:29 +0500

    third-party-libraries - YoutubeEmbeded

--- third_party_libraries/app/page.js
+++ third_party_libraries/app/page.js
@@ -2,7 +2,11 @@ import EventButton from "./components/sendGAEvent";
 
 export default function Page() {
   return (
-   
+    <div className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h2 className="mb-4 text-2xl font-bold">
+        Welcome to the our Event Tracker
+      </h2>
+      <EventButton />
     </div>
   );
 }
