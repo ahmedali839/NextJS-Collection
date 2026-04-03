bffb4f5981d2815955275cb476531876b4299d0d
ahmedali839 <ahmedyarr1212@gmail.com>
2026-04-03 12:57:46 +0500

    href, prefech, blocking-navigation feature of <Link>

--- link_component/proxy.js
+++ link_component/proxy.js
@@ -1,4 +1,6 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
+
+// code for only authenticated users accesss to dashboard route or handling unAuthenticated
 
 export function proxy(request) {
   const nextUrl = request.nextUrl;
