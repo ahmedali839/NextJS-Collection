fe1aa987ac629b7dd7f36c20335c876fc2ae5577
ahmedali839 <ahmedyarr1212@gmail.com>
2026-04-05 23:07:58 +0500

    cacheTag, use-cache

--- functions/app/dashboard/page.js
+++ functions/app/dashboard/page.js
@@ -1,5 +1,22 @@
-import { cacheLife } from "next/cache";
+import { cacheLife, cacheTag } from "next/cache";
 import { Widget } from "./widget";
+import { Suspense } from "react";
+
+export async function FetchData() {
+  "use cache";
+  cacheTag("json-data");
+  // no 3s waiting, bcz it's cached but for first time
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/2");
+
+  if (!res.ok) {
+    return <p>Failed to load user data.</p>;
+  }
+
+  const user = await res.json();
+
+  return <p>User: {user?.name ?? "Unknown"}</p>;
+}
 
 export default async function Dashboard() {
   "use cache";
@@ -12,6 +29,10 @@ export default async function Dashboard() {
       <h1>Dashboard</h1>
       <p>Dashboard Time: {new Date().toLocaleTimeString()}</p>
       <Widget />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <FetchData />
+      </Suspense>
     </div>
   );
 }
