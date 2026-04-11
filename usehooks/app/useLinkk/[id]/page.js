106c71195b652e89e2d7029cd97e888cd60be4a8
Ahmed <ahmedyarr1212@gmail.com>
2026-04-11 23:44:09 +0500

    usehooks built-in nextjs

--- usehooks/app/useLinkk/[id]/page.js
+++ usehooks/app/useLinkk/[id]/page.js
@@ -1,7 +1,6 @@
 export default async function page({ params }) {
   const { id } = await params;
 
-  // it will wait for 5 seconds before rendering the page, you can remove this line if you don't want to wait
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return <div>Child Page id: {id} </div>;
