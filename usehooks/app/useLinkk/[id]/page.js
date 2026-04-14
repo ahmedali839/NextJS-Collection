785560bb238f00c3298bc3ab30bf9dd55a1e1d1c
Ahmed Yar <141534802+ahmedali839@users.noreply.github.com>
2026-04-14 23:55:01 +0500

    Params in server component

--- usehooks/app/useLinkk/[id]/page.js
+++ usehooks/app/useLinkk/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function page({ params }) {
-  const { id } = await params;
+  const { id } = await params; // getting the id as params
 
   await new Promise((resolve) => setTimeout(resolve, 5000));
 // learning usehooks in next.js
