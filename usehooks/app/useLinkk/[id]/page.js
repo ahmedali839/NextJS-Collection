d293bbb51b66149c40d2c7c7a86dafa23e8c370c
Ahmed Yar <141534802+ahmedali839@users.noreply.github.com>
2026-04-14 23:53:41 +0500

    Learning hooks in react

--- usehooks/app/useLinkk/[id]/page.js
+++ usehooks/app/useLinkk/[id]/page.js
@@ -2,7 +2,7 @@ export default async function page({ params }) {
   const { id } = await params;
 
   await new Promise((resolve) => setTimeout(resolve, 5000));
-// learning usehooks
+// learning usehooks in next.js
   
   return <div>Child Page id: {id} </div>;
 }
