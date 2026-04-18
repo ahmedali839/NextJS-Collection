a9c0482059045337436f360c43c8d4778828fa7c
Ahmed Yar <141534802+ahmedali839@users.noreply.github.com>
2026-04-18 23:38:45 +0500

    use-cache

--- use-cache/src/app/about/page.jsx
+++ use-cache/src/app/about/page.jsx
@@ -5,7 +5,7 @@ import { refreshAboutCache } from "../lib/actions";
 export default function About() {
   return (
     <main className="p-10 font-sans">
-      <h2 className="text-2xl font-bold">About Us</h2>
+      <h2 className="text-2xl font-bold">About Us</h2> // function
 
       <Suspense fallback={<p>Loading...</p>}>
         <AboutContent />
