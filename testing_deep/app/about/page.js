b2ca0f84c9558ef6acd71bf61840a23eda7c511f
ahmedali839 <ahmedyarr1212@gmail.com>
2026-04-22 12:27:24 +0500

    Component and E2E testing

---
+++ testing_deep/app/about/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  cvc;
+  return (
+    <div>
+      <h2>About page</h2>
+      <Link href="/ab" className="cursor-alias text-blue-500">
+        Home
+      </Link>
+    </div>
+  );
+};
+
+export default page;
