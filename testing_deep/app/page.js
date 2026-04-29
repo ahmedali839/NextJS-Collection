05a68ef4342a6cfd595d24e5a9d69c5183dc9fc0
Ahmed <ahmedyarr1212@gmail.com>
2026-04-29 23:58:07 +0500

    core concepts

--- testing_deep/app/page.js
+++ testing_deep/app/page.js
@@ -3,7 +3,7 @@ import { Poppins, ABeeZee, Cedarville_Cursive } from "next/font/google";
 import Link from "next/link";
 
 const cursive = Cedarville_Cursive({
-  weight: "400", // beautifull fonts
+  weight: "400", // beautifull fonts for website
 });
 export default function Home() {
   return (
