51b380a7f17c356f59af9f7bd71a71320e539ff9
Ahmed <ahmedyarr1212@gmail.com>
2026-05-11 20:10:33 +0500

    backend:validates, imageKit config

--- next_auth/app/components/accessToken.js
+++ next_auth/app/components/accessToken.js
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react";
 
 export default function Component() {
-  const { data: session, status } = useSession();
+  const { data: session, status } = useSession(); // for getting session
 
   if (status === "loading") {
     return <div>Loading access token...</div>;
